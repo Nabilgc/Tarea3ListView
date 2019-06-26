e8bcc98423884ad66c29e8ba9a374ef68896aac3
Nabil Galeb <nabilgaleb@hotmail.com>
2019-06-25 23:23:20 -0400

    Commit Car y CarsPage

---
+++ TareaListView/TareaListView/TareaListView/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaListView.Models
+{
+    public class Car
+    {
+
+        public int CarID { get; set; }
+        public string Make { get; set; }
+        public int YearOfModel { get; set; }
+
+    }
+}
