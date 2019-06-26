43e1cba02ce514b822a7b84dc0bc7fc6d75ed0c3
Nabil Galeb <nabilgaleb@hotmail.com>
2019-06-25 23:30:04 -0400

    ViewModel y Utils

--- TareaListView/TareaListView/TareaListView/Views/CarsPage.xaml.cs
+++ TareaListView/TareaListView/TareaListView/Views/CarsPage.xaml.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TareaListView.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TareaListView.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CarsPage : ContentPage
-	{
-		public CarsPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    public partial class CarsPage : ContentPage
+    {
+
+        public CarsPage()
+        {
+
+            InitializeComponent();
+
+            // Connecting context of this page to the our View Model class
+            BindingContext = new CarsViewModel();
+        }
+    }
 }
